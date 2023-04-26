4523bab0b27f4adb998fba777a78a999bb6a05c5
YukoSawai <129607371 +yuko-sawa@users.noreply.github.com>
2023-04-26 07:45:37 +0000

    bootstrap OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  
+  resources :book,only:[:new, :index, :show]
+  
   get 'homes/about' => 'homes#about', as: 'about'
+  
   
 end
